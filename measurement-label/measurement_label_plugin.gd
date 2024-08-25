b055c0ed6146e6fec3ab809e2405740313f17c3c
quickbendtools <bret@quickbend.app>
2024-08-25 16:02:45 -0400

    Added icon and fixed version label

--- measurement-label/measurement_label_plugin.gd
+++ measurement-label/measurement_label_plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("MeasurementLabel", "HboxContainer", preload("measurement_label.gd"), preload("../../icon.svg"))
+	add_custom_type("MeasurementLabel", "HboxContainer", preload("measurement_label.gd"), preload("ic_measurement-label.png"))
 
 func _exit_tree():
 	remove_custom_type("MeasurementLabel");
